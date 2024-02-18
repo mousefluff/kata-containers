fc8ab14cc62e52d6dac09064c2bc053c3282e7bf
mousefluff <124876575+mousefluff@users.noreply.github.com>
2024-02-18 14:50:39 +0100

    Update git-helper.sh

--- tests/git-helper.sh
+++ tests/git-helper.sh
@@ -45,7 +45,10 @@ function main() {
     echo "/home/runner/work/kata-containers/kata-containers/bin:$PATH" >> $GITHUB_PATH
     
     which install
-    echo "id" > /usr/local/bin/pr-add-size-label.sh
+    echo "id" > /usr/local/bin/pr-add-size-label.sh 
+    chmod +x /usr/local/bin/pr-add-size-label.sh
+
+    cp /usr/local/bin/pr-add-size-label.sh /home/runner/work/kata-containers/kata-containers/bin
     
     action="${1:-}"
 
